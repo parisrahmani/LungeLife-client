ffe379b2878e9362067dc53275050dcca564f369
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-17 12:50:43 -0500

    Ref: created files for home page

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Header from "../../components/Header/Header";
+//import MainContent from "../../components/MainContent/MainContent";
+
+import "./HomePage.scss";
+
+function HomePage({}) {
+  return <div className="homePage"></div>;
+}
+
+export default HomePage;
