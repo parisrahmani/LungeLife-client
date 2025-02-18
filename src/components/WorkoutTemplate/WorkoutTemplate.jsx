39848b231ed59577cb2010cf2cbcf1587dcc652c
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-18 00:05:35 -0500

    Dev: write the function for sessions

---
+++ src/components/WorkoutTemplate/WorkoutTemplate.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+import "./WorkoutTemplate.scss";
+
+function WorkoutTemplate() {
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/sessions")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setTemplates(data);
+      })
+      .catch((err) => console.error("Error fetching templates:", err));
+  }, []);
+
+  //setTemplates([...templates, data]); // Update state
+
+  return (
+    <section className="templateSection">
+      <h1 className="templateSection__title">Sample Sessions</h1>
+      <div className="templateCard">
+        <ul className="templateCard__list">
+          {templates.map((template) => (
+            <li key={template.id} className="templateCard__item">
+              <div className="templateCard__section">
+                <div className="templateCard__title">
+                  <h3 className="templateCard__name">
+                    {template.template_name}
+                  </h3>
+                  {/* <p className="templateCard__date">{template.date}</p> */}
+                </div>
+                {/* <h4>Exercises: </h4> */}
+                <ul className="templateCard__exerciseList">
+                  {Array.isArray(template.exercises)
+                    ? template.exercises.map((exercise, index) => (
+                        <li key={index} className="templateCard__exerciseItem">
+                          {exercise}
+                        </li>
+                      ))
+                    : template.exercises
+                    ? JSON.parse(template.exercises).map((exercise, index) => (
+                        <li key={index}>{exercise}</li>
+                      ))
+                    : "No exercises listed"}
+                </ul>
+                <p> Notes: {template.notes}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default WorkoutTemplate;
