87d1cff780f67748f58b33698c73463de780bcf5
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-21 19:23:36 -0500

    Fix: add eeded props to re-featch templates without reloading page

--- src/components/WorkoutTemplate/WorkoutTemplate.jsx
+++ src/components/WorkoutTemplate/WorkoutTemplate.jsx
@@ -3,73 +3,68 @@ import { Link } from "react-router-dom";
 
 import "./WorkoutTemplate.scss";
 
-function WorkoutTemplate({ reloadtempaltes }) {
-  const [templates, setTemplates] = useState([]);
-
+function WorkoutTemplate({ templates, reloadTempaltes, setTemplates }) {
   useEffect(() => {
     async function getTemplates() {
       try {
         const response = await fetch("http://localhost:8080/api/templates");
         const data = await response.json();
         console.log("Fetched data:", data);
-        setTemplates(data);
+        const sortedData = data.sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+        );
+        setTemplates(sortedData);
       } catch (err) {
         console.error("Error fetching templates:", err);
       }
     }
 
     getTemplates();
-  }, [reloadtempaltes]);
-
-  // useEffect(() => {
-  //   fetch("http://localhost:8080/api/templates")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log("Fetched data:", data);
-  //       setTemplates(data);
-  //     })
-  //     .catch((err) => console.error("Error fetching templates:", err));
-  // }, [reloadtempaltes]);
-
-  //setTemplates([...templates, data]); // Update state
+  }, [reloadTempaltes]);
 
   return (
     <section className="templateSection">
       <h1 className="templateSection__title">Sample Templates</h1>
       <div className="templateCard">
         <ul className="templateCard__list">
-          {templates.map((template) => (
-            <li key={template.id} className="templateCard__item">
-              <Link to={`/start/${template.id}`} className="templateCard__link">
-                <div className="templateCard__section">
-                  <div className="templateCard__title">
-                    <h3 className="templateCard__name">
-                      {template.template_name}
-                    </h3>
-                    {/* <p className="templateCard__date">{template.date}</p> */}
+          {templates &&
+            templates.map((template) => (
+              <li key={template.id} className="templateCard__item">
+                <Link
+                  to={`/start/${template.id}`}
+                  className="templateCard__link"
+                >
+                  <div className="templateCard__section">
+                    <div className="templateCard__title">
+                      <h3 className="templateCard__name">
+                        {template.template_name}
+                      </h3>
+                      <p className="templateCard__note">
+                        {" "}
+                        Notes: {template.notes}
+                      </p>
+                    </div>
+                    {/* <h4>Exercises: </h4> */}
+                    <ul className="templateCard__exerciseList">
+                      {Array.isArray(template.exercises)
+                        ? template.exercises.map((exercise, index) => (
+                            <li
+                              key={index}
+                              className="templateCard__exerciseItem"
+                            >
+                              {exercise}
+                            </li>
+                          ))
+                        : template.exercises
+                        ? JSON.parse(template.exercises).map(
+                            (exercise, index) => <li key={index}>{exercise}</li>
+                          )
+                        : "No exercises listed"}
+                    </ul>
                   </div>
-                  {/* <h4>Exercises: </h4> */}
-                  <ul className="templateCard__exerciseList">
-                    {Array.isArray(template.exercises)
-                      ? template.exercises.map((exercise, index) => (
-                          <li
-                            key={index}
-                            className="templateCard__exerciseItem"
-                          >
-                            {exercise}
-                          </li>
-                        ))
-                      : template.exercises
-                      ? JSON.parse(template.exercises).map(
-                          (exercise, index) => <li key={index}>{exercise}</li>
-                        )
-                      : "No exercises listed"}
-                  </ul>
-                  <p> Notes: {template.notes}</p>
-                </div>
-              </Link>
-            </li>
-          ))}
+                </Link>
+              </li>
+            ))}
         </ul>
       </div>
     </section>
