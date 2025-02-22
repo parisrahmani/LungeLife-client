04b9da823c060ef777227e8e361f1775e25e05cc
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-21 19:20:36 -0500

    Ref: removed dumbell icon from sign-in

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,67 +1,7 @@
-// import React, { useState } from "react";
-// import "./SignInPage.scss";
-
-// function SignInPage() {
-//   const [isSignUp, setIsSignUp] = useState(false);
-
-//   const toggleForm = () => {
-//     setIsSignUp(!isSignUp);
-//   };
-
-//   return (
-//     <div className="auth">
-//       <div className="auth__form-container">
-//         <h2 className="auth__title">{isSignUp ? "Sign Up" : "Sign In"}</h2>
-//         <form className="auth__form">
-//           <div className="auth__input-group">
-//             <label htmlFor="email" className="auth__label">
-//               Email
-//             </label>
-//             <input type="email" id="email" className="auth__input" required />
-//           </div>
-//           <div className="auth__input-group">
-//             <label htmlFor="password" className="auth__label">
-//               Password
-//             </label>
-//             <input
-//               type="password"
-//               id="password"
-//               className="auth__input"
-//               required
-//             />
-//           </div>
-//           {isSignUp && (
-//             <div className="auth__input-group">
-//               <label htmlFor="confirm-password" className="auth__label">
-//                 Confirm Password
-//               </label>
-//               <input
-//                 type="password"
-//                 id="confirm-password"
-//                 className="auth__input"
-//                 required
-//               />
-//             </div>
-//           )}
-//           <button type="submit" className="auth__button">
-//             {isSignUp ? "Sign Up" : "Sign In"}
-//           </button>
-//         </form>
-//         <p className="auth__toggle-text">
-//           {isSignUp ? "Already have an account?" : "Don’t have an account?"}
-//           <span onClick={toggleForm} className="auth__toggle-link">
-//             {isSignUp ? "Sign In" : "Sign Up"}
-//           </span>
-//         </p>
-//       </div>
-//     </div>
-//   );
-// }
-// export default SignInPage;
-
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import "./SignInPage.scss";
-import { FaDumbbell } from "react-icons/fa"; // Adding a fitness icon
 
 const SignInSignUp = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -73,27 +13,19 @@ const SignInSignUp = () => {
   return (
     <div className="auth">
       <div className="auth__form-container">
-        <div className="auth__logo">
-          <FaDumbbell className="auth__icon" />
-          <h2 className="auth__title">{isSignUp ? "Sign Up" : "Sign In"}</h2>
-        </div>
+        <h2 className="auth__title">{isSignUp ? "Sign Up" : "Sign In"}</h2>
         <form className="auth__form">
           <div className="auth__input-group">
             <label htmlFor="email" className="auth__label">
               Email
             </label>
-            <input type="email" id="email" className="auth__input" required />
+            <input type="email" id="email" className="auth__input" />
           </div>
           <div className="auth__input-group">
             <label htmlFor="password" className="auth__label">
               Password
             </label>
-            <input
-              type="password"
-              id="password"
-              className="auth__input"
-              required
-            />
+            <input type="password" id="password" className="auth__input" />
           </div>
           {isSignUp && (
             <div className="auth__input-group">
@@ -104,13 +36,14 @@ const SignInSignUp = () => {
                 type="password"
                 id="confirm-password"
                 className="auth__input"
-                required
               />
             </div>
           )}
-          <button type="submit" className="auth__button">
-            {isSignUp ? "Sign Up" : "Sign In"}
-          </button>
+          <Link to="/">
+            <button type="submit" className="auth__button">
+              {isSignUp ? "Sign Up" : "Sign In"}
+            </button>
+          </Link>
         </form>
         <p className="auth__toggle-text">
           {isSignUp ? "Already have an account?" : "Don’t have an account?"}
