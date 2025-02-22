df8e71fbeea17655a7f06e7f17c37435add9e7ec
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-21 19:18:50 -0500

    Ref: moved timer to a seprate component

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import "./Timer.scss";
+
+function Timer() {
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime((prev) => prev + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const formatTime = (seconds) => {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs.toString().padStart(2, "0")}`;
+  };
+
+  const toggleTimer = () => {
+    setIsRunning((prev) => !prev);
+  };
+
+  const resetTimer = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+  return (
+    <div>
+      <div className="timer">
+        {formatTime(elapsedTime)}
+        <button onClick={toggleTimer} className="timer__button">
+          {isRunning ? "Pause" : "Start"}
+        </button>
+        <button onClick={resetTimer} className="timer__button reset">
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
--- src/pages/StartWorkoutPage/StartWorkoutPage.jsx
+++ src/pages/StartWorkoutPage/StartWorkoutPage.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+
 import ExerciseModal from "../../components/ExerciseModal/ExerciseModal";
 import ExerciseForm from "../../components/ExerciseForm/ExerciseForm";
+import Header from "../../components/Header/Header";
+import Timer from "../../components/Timer/Timer";
 
 import "./StartWorkoutPage.scss";
 
@@ -11,9 +14,9 @@ function StartWorkoutPage() {
   const [selectedExercises, setSelectedExercises] = useState([]);
   const [addedExercises, setAddedExercises] = useState([]);
 
-  const [selectedDate, setSelectedDate] = useState("");
-  const [elapsedTime, setElapsedTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  // const [selectedDate, setSelectedDate] = useState("");
+  // const [elapsedTime, setElapsedTime] = useState(0);
+  // const [isRunning, setIsRunning] = useState(false);
 
   const { id } = useParams();
 
@@ -46,55 +49,39 @@ function StartWorkoutPage() {
     setShowModal(false);
   };
 
-  useEffect(() => {
-    let interval;
-    if (isRunning) {
-      interval = setInterval(() => {
-        setElapsedTime((prev) => prev + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isRunning]);
+  // useEffect(() => {
+  //   let interval;
+  //   if (isRunning) {
+  //     interval = setInterval(() => {
+  //       setElapsedTime((prev) => prev + 1);
+  //     }, 1000);
+  //   } else {
+  //     clearInterval(interval);
+  //   }
+  //   return () => clearInterval(interval);
+  // }, [isRunning]);
 
-  const formatTime = (seconds) => {
-    const mins = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${mins}:${secs.toString().padStart(2, "0")}`;
-  };
+  // const formatTime = (seconds) => {
+  //   const mins = Math.floor(seconds / 60);
+  //   const secs = seconds % 60;
+  //   return `${mins}:${secs.toString().padStart(2, "0")}`;
+  // };
 
-  const toggleTimer = () => {
-    setIsRunning((prev) => !prev);
-  };
+  // const toggleTimer = () => {
+  //   setIsRunning((prev) => !prev);
+  // };
 
-  const resetTimer = () => {
-    setIsRunning(false);
-    setElapsedTime(0);
-  };
+  // const resetTimer = () => {
+  //   setIsRunning(false);
+  //   setElapsedTime(0);
+  // };
 
   return (
     <div className="start-workout">
+      <Header />
       <div className="start-workout__header">
         <h1 className="start-workout__title">Start Workout</h1>
-
-        <div className="start-workout__timer">
-          {formatTime(elapsedTime)}
-          <button onClick={toggleTimer} className="start-workout__button">
-            {isRunning ? "Pause" : "Start"}
-          </button>
-          <button onClick={resetTimer} className="start-workout__button reset">
-            Reset
-          </button>
-        </div>
-      </div>
-      <div className="start-workout__date">
-        {/* <input
-          type="date"
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-          className="start-workout__date-input"
-        /> */}
+        <Timer />
       </div>
 
       {showModal && (
@@ -109,13 +96,16 @@ function StartWorkoutPage() {
       {addedExercises.map((exercise, index) => (
         <ExerciseForm key={index} exercise={exercise} />
       ))}
-
-      <button onClick={handleOpenModal} className="start-workout__button-add">
-        Add Exercises
-      </button>
-      <Link to="/">
-        <button className="start-workout__button-cancel">Cancel Workout</button>
-      </Link>
+      <div className="start-workout__button-container">
+        <button onClick={handleOpenModal} className="start-workout__button-add">
+          Add Exercises
+        </button>
+        <Link to="/">
+          <button className="start-workout__button-cancel">
+            Cancel Workout
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
